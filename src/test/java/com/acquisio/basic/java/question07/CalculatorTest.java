1e9c807e7c40fae952d68f759f5c5d3b2db31150
maxbuteau <46498123+maxbuteau@users.noreply.github.com>
2019-04-07 15:11:30 -0400

    Update CalculatorTest.java

--- src/test/java/com/acquisio/basic/java/question07/CalculatorTest.java
+++ src/test/java/com/acquisio/basic/java/question07/CalculatorTest.java
@@ -1,6 +1,36 @@
 package com.acquisio.basic.java.question07;
 
-public class CalculatorTest {
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-    // TODO: Insert your code here.
+public class CalculatorTest {
+	@Test
+	public void twoPlusTwo_ReturnsFour() {
+		Integer expected = 4;
+		Integer actual = Calculator.applyOperation(2, 2, '+');
+		assertEquals(actual, expected);
+	}
+	@Test
+	public void oneMinusThree_ReturnsMinusTwo() {
+		Integer expected = -2;
+		Integer actual = Calculator.applyOperation(1, 3, '-');
+		assertEquals(actual, expected);
+	}
+	@Test
+	public void divideByZero_ReturnsNull() {
+		Integer expected = null;
+		Integer actual = Calculator.applyOperation(1, 0, '/');
+		assertEquals(actual, expected);
+	}
+	@Test
+	public void unknownOperator_throwsIllegalArgumentException() {
+		char operator = '&';
+		try {
+			Calculator.applyOperation(2, 3, operator);
+		} catch (IllegalArgumentException e) {
+			assertTrue(e.getMessage().trim().contains("Operator '" +operator+ "' not supported"));
+		}
+	}
+
 }
+
